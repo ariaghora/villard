01d325f24ad8f67df5b2bb21247352c6cc2dc028
Aria Ghora Prabono <hello@ghora.net>
2022-07-12 23:56:27 +0700

    villard: refactor private methods

--- villard/villlard.py
+++ villard/villlard.py
@@ -18,7 +18,7 @@ DATA_CATALOG_PREFIX = "data::"
 
 class Villard:
     def __new__(cls):
-        """Create a new instance of the class as a singleton."""
+        # Create a new instance of the class as a singleton.
         if not hasattr(cls, "instance"):
             cls.instance = super(Villard, cls).__new__(cls)
 
@@ -29,6 +29,7 @@ class Villard:
         cls.supported_data_types = ["DT_PICKLE", "DT_PANDAS_DATAFRAME"]
         cls.supported_data_writer = [PickleWriter, PandasWriter]
         cls.supported_data_reader = [PickleReader, PandasReader]
+
         cls.type_to_reader_map = {
             k: v for k, v in zip(cls.supported_data_types, cls.supported_data_reader)
         }
@@ -41,14 +42,14 @@ class Villard:
 
         return cls.instance
 
-    def visit_and_execute_recursively(cls, name, node, stats_table):
+    def _visit_and_execute_recursively(cls, name, node, stats_table):
         if node["executed"]:
             return
 
         # Before executing function of current node, execute all the nodes that are
         # dependencies of the current node recursively.
         for prev in node["prevs"]:
-            cls.visit_and_execute_recursively(
+            cls._visit_and_execute_recursively(
                 prev, cls.execution_nodes[prev], stats_table
             )
 
@@ -77,7 +78,34 @@ class Villard:
         print(colored(f"⦿ Completed `{name}`", "green"))
 
         execution_time = toc - tic
-        stats_table.append((name, execution_time, 34))
+        dependencies = node["prevs"]
+
+        stats_table.append((name, dependencies, execution_time))
+
+    def _get_data_info(cls, data_catalog_key):
+        try:
+            data_info = cls.data_catalog[data_catalog_key]
+        except KeyError:
+            msg = f"Data catalog key `{data_catalog_key}` is not defined."
+            print(colored("Error:", "red"), colored(msg, "red"))
+            exit(1)
+
+        return data_info
+
+    def _get_data_type(cls, data_info: Dict[str, Any], data_catalog_key: str):
+        try:
+            data_type = data_info["type"]
+        except KeyError:
+            msg = f"Cannot determine the type of data `{data_catalog_key}`."
+            print(colored("Error:", "red"), colored(msg, "red"))
+            exit(1)
+
+        if not data_type in cls.supported_data_types:
+            msg = f"Data type `{data_type}` is not supported."
+            print(colored("Error:", "red"), colored(msg, "red"))
+            exit(1)
+
+        return data_type
 
     def node(cls, name: str):
         def decorator_node(func):
@@ -110,13 +138,13 @@ class Villard:
                 print(colored("Error:", "red"), colored(msg, "red"))
                 exit(1)
 
-        """ To keep track nodes' incoming and outgoing edge counts """
+        # To keep track nodes' incoming and outgoing edge counts
         for name in cls.pipeline_definition:
             cls.execution_nodes_in_out_counter[name] = dict()
             cls.execution_nodes_in_out_counter[name]["in"] = 0
             cls.execution_nodes_in_out_counter[name]["out"] = 0
 
-        """ Build execution graph """
+        # Build execution graph
         for name, kwargs in cls.pipeline_definition.items():
             execution_node = dict()
             execution_node["func"] = cls.node_func_map[name]
@@ -128,26 +156,32 @@ class Villard:
                 # if "ref" key is detected, it means that the node refers to another node.
                 # Add the referenced node into the dependency list (`prevs`) of the current node.
                 for k, v in kwargs.items():
-                    if k == "ref":
-                        execution_node["prevs"].append(v)
-                        cls.execution_nodes_in_out_counter[name]["in"] += 1
-                        cls.execution_nodes_in_out_counter[v]["out"] += 1
+                    if isinstance(v, str):
+                        if v.startswith(REFERENCE_PREFIX):
+                            prev_name = v.lstrip(REFERENCE_PREFIX)
+                            execution_node["prevs"].append(prev_name)
+                            cls.execution_nodes_in_out_counter[name]["in"] += 1
+                            cls.execution_nodes_in_out_counter[prev_name]["out"] += 1
                     elif isinstance(v, dict):
                         check_ref_recursively(v)
 
             check_ref_recursively(kwargs)
             cls.execution_nodes[name] = execution_node
 
-        """ Output nodes are the ones that have no outging edges """
+        # Collect output nodes: the ones that have no outging edges
         output_nodes = {
             node: v
             for node, v in cls.execution_nodes.items()
             if cls.execution_nodes_in_out_counter[node]["out"] == 0
         }
 
+        # Keep track execution statistics. This will be displayed as table in the end
+        # of the execution.
         stats_table = []
+        stats_table_headers = ["Node", "Dependencies", "Execution Time"]
+
         for output_node_name in output_nodes:
-            cls.visit_and_execute_recursively(
+            cls._visit_and_execute_recursively(
                 output_node_name, cls.execution_nodes[output_node_name], stats_table
             )
 
@@ -155,39 +189,14 @@ class Villard:
             "\n"
             + tabulate(
                 stats_table,
-                headers=["Node", "Time", "Output Path"],
+                headers=stats_table_headers,
                 tablefmt="fancy_grid",
             )
         )
 
-    def get_data_info(cls, data_catalog_key):
-        try:
-            data_info = cls.data_catalog[data_catalog_key]
-        except KeyError:
-            msg = f"Data catalog key `{data_catalog_key}` is not defined."
-            print(colored("Error:", "red"), colored(msg, "red"))
-            exit(1)
-
-        return data_info
-
-    def get_data_type(cls, data_info: Dict[str, Any], data_catalog_key: str):
-        try:
-            data_type = data_info["type"]
-        except KeyError:
-            msg = f"Cannot determine the type of data `{data_catalog_key}`."
-            print(colored("Error:", "red"), colored(msg, "red"))
-            exit(1)
-
-        if not data_type in cls.supported_data_types:
-            msg = f"Data type `{data_type}` is not supported."
-            print(colored("Error:", "red"), colored(msg, "red"))
-            exit(1)
-
-        return data_type
-
     def read_data(cls, data_catalog_key: str):
-        data_info = cls.get_data_info(data_catalog_key)
-        data_type = cls.get_data_type(data_info, data_catalog_key)
+        data_info = cls._get_data_info(data_catalog_key)
+        data_type = cls._get_data_type(data_info, data_catalog_key)
 
         # Try to get read parameters
         try:
@@ -199,8 +208,8 @@ class Villard:
         return reader.read_data(data_info["path"], **kwargs)
 
     def write_data(cls, data_catalog_key: str, data: object):
-        data_info = cls.get_data_info(data_catalog_key)
-        data_type = cls.get_data_type(data_info, data_catalog_key)
+        data_info = cls._get_data_info(data_catalog_key)
+        data_type = cls._get_data_type(data_info, data_catalog_key)
 
         # Try to get write parameters
         try:
